842cf51072ca6ea7af90948f7c0297011151c169
maheshwarikarankot <maheshwari635@gmail.com>
2026-04-09 23:48:08 -0500

    Revert "made changes in the implementation of article,comment,category,user,common folders"
    This reverts commit 45d1636540e0b39f596971381422a351090681b1.

--- src/user/entities/user.entity.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: string; // uuid v4
-  login: string;
-  password: string;
-  role: 'admin' | 'editor' | 'viewer';
-  createdAt: number; // timestamp of creation
-  updatedAt: number; // timestamp of last update
-}
