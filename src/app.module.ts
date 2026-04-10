562630cce5bb818ab890d6d86dbb76198ad39f50
maheshwarikarankot <maheshwari635@gmail.com>
2026-04-10 12:12:53 -0500

    "made changes in the implementation of article,comment,category,user,common folders"""
    This reverts commit 2cce3d23945c5189a5a3b0ce66de034b1cfc2f73.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { CategoryModule } from './category/category.module';
-import { ArticleModule } from './article/article.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CommentModule } from './comment/comment.module';
-import { LoggingInterceptor } from './common/logging.interceptor';
 
 @Module({
-  imports: [UserModule, CategoryModule, ArticleModule, CommentModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/comment/entities/comment.entity.ts
+++
@@ -1,7 +0,0 @@
-export interface Comment {
-  id: string; // uuid v4
-  content: string;
-  articleId: string; // refers to Article
-  authorId: string | null; // refers to User
-  createdAt: number; // timestamp of creation
-}
