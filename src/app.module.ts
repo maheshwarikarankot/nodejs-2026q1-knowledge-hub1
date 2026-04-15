6b2971558713e364e7ff1825d182b49084aea83e
maheshwarikarankot <maheshwari635@gmail.com>
2026-04-15 18:54:05 -0500

    updated app module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,21 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ArticleModule } from './article/article.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD } from '@nestjs/core';
 import { CommentModule } from './comment/comment.module';
 import { LoggingInterceptor } from './common/logging.interceptor';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthRbacGuard } from './auth/guards/auth-rbac.guard';
 
 @Module({
-  imports: [UserModule, CategoryModule, ArticleModule, CommentModule, PrismaModule],
+  imports: [UserModule, CategoryModule, ArticleModule, CommentModule, PrismaModule, AuthModule, JwtModule.register({})],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_GUARD, useClass: AuthRbacGuard },
+  ],
 })
 export class AppModule {}
