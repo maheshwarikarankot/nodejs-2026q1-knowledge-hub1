8d6fbf8c69997ab7253b1f888af39aba768ddf55
maheshwarikarankot <maheshwari635@gmail.com>
2026-04-27 13:27:10 -0500

    updated vitest.config.ts file include: ['src/unit/**/*.spec.ts']

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    include: ['test/unit/**/*.spec.ts'],
+    include: ['src/unit/**/*.spec.ts'],
     setupFiles: ['reflect-metadata'],
     clearMocks: true,
     restoreMocks: true,
